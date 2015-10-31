f19316385bb53102c67c86138ce062513387a716
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-31 03:46:18 -0400

    adds configs array for debugging

---
+++ test/support/configs.js
@@ -0,0 +1,30 @@
+var normalize = require('./');
+var configs = [
+  ['*.js'],
+  ['*.js', 'foo/'],
+  ['*.js', ''],
+  [['*.js'], ''],
+  ['*.js', 'foo/', {cwd: ''}],
+  [['*.js']],
+  [['foo', 'bar', '*.js']],
+  [{'foo/': '*.js', 'bar/': '*.md'}],
+  [{'foo/': '*.js', cwd: 'a'}],
+  [{'foo/': '*.js'}],
+  [{'': '*.js'}],
+  [{'a/b/c': '*.js'}, {cwd: 'foo'}],
+  [{dest: '', src: '*.js', options: {cwd: 'b/'}}],
+  [{dest: 'a/', src: '*.js', options: {cwd: 'b/'}}],
+  [{dest: 'foo/', src: '*.js', cwd: 'b'}],
+  [{dest: 'foo/', src: '*.js'}],
+  [{dest: 'foo/', src: '*.js'}, {process: true}],
+  [{dest: 'foo/', src: ['*.js'], options: {cwd: 'c'}}],
+  [{dest: 'out/'}],
+  [{files: ['*.js']}],
+  [{files: [{'foo/': '*.js', cwd: 'f'}]}],
+  [{files: {'dist/': ['*.js']}}],
+  [{files: {'foo/': '*.js', cwd: 'e'}}],
+  [{options: {src: '*.js', dest: 'foo/'}}],
+].forEach(function (config) {
+  var res = normalize.apply(null, config);
+  console.log(res.files);
+});
