31b1c6c00331f831c049a35d60d21565bf85d845
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-05 23:47:43 -0500

    fixes bug where certain arbitrary properties weren't copied

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ function normalize(config, dest, options) {
     config = toObject(config, dest, options);
   }
 
-  var thisArg = {};
   var res = null;
-
-  if (utils.isObject(this) && this.options) {
-    thisArg = this.options;
-  }
-
   switch(typeOf(config)) {
     case 'string':
       res = normalizeString(config);
@@ -31,125 +25,14 @@ function normalize(config, dest, options) {
     }
   }
 
+  var thisArg = {};
+  if (utils.isObject(this) && this.options) {
+    thisArg = this.options;
+  }
+
   // copy `thisArg` options onto config root
   copyOptions(res, thisArg);
   return formatObject(res);
-}
-
-/**
- * Convert args list to a config object.
- */
-
-function toObject(src, dest, options) {
-  if (!utils.isObject(options)) {
-    options = {};
-  }
-
-  var config = {};
-
-  if (utils.isObject(dest)) {
-    options = extend({}, options, dest);
-    dest = null;
-  }
-
-  if (utils.isObject(src)) {
-    config = src;
-  }
-
-  if (isValidSrc(src)) {
-    config.src = src;
-  }
-
-  if (typeof dest === 'string') {
-    config.dest = dest;
-  }
-
-  if (options.hasOwnProperty('options')) {
-    options = extend({}, options, options.options);
-    delete options.options;
-  }
-
-  config.options = extend({}, config.options, options);
-  return config;
-}
-
-/**
- * Object
- */
-
-function normalizeObject(val) {
-  if (val.hasOwnProperty('path')) {
-    val.src = val.path;
-  }
-
-  val = normalizeOptions(val);
-
-  // if src/dest are on options, move them to root
-  val = utils.move(val, 'src');
-  val = utils.move(val, 'dest');
-
-  // allow src to be a getter
-  if ('src' in val || val.hasOwnProperty('dest')) {
-    return toFiles(val);
-  }
-
-  if (!val.hasOwnProperty('files')) {
-    return filesObjects(val);
-  }
-
-  if (Array.isArray(val.files)) {
-    val.files = reduceFiles(val.files);
-    return val;
-  }
-
-  if (val.files && !utils.isObject(val.files)) {
-    throw new TypeError('expected "files" to be an array or object');
-  }
-
-  val.files = normalizeFiles(val);
-  return val;
-}
-
-/**
- * String
- */
-
-function normalizeString(val) {
-  return toFiles({src: [val]});
-}
-
-/**
- * Array
- */
-
-function normalizeArray(arr) {
-  if (isValidSrc(arr[0])) {
-    return toFiles({src: arr});
-  }
-  return {files: reduceFiles(arr)};
-}
-
-/**
- * Files property
- */
-
-function normalizeFiles(val) {
-  var res = normalize(val.files);
-  return res.files;
-}
-
-/**
- * Normalize all objects in a `files` array.
- *
- * @param {Array} `files`
- * @return {Array}
- */
-
-function reduceFiles(files) {
-  return files.reduce(function(acc, ele) {
-    var res = normalize(ele);
-    return acc.concat(res.files);
-  }, []);
 }
 
 /**
@@ -178,6 +61,170 @@ function copyOptions(config, thisArg) {
 }
 
 /**
+ * Convert args list to a config object.
+ */
+
+function toObject(src, dest, options) {
+  if (!utils.isObject(options)) {
+    options = {};
+  }
+
+  var config = {};
+  if (utils.isObject(dest)) {
+    options = extend({}, options, dest);
+    dest = null;
+  }
+
+  if (utils.isObject(src)) {
+    config = src;
+  }
+
+  if (isValidSrc(src)) {
+    config.src = src;
+  } else if (Array.isArray(src)) {
+    config = normalize(src);
+  }
+
+  if (typeof dest === 'string') {
+    config.dest = dest;
+  }
+
+  if (options.hasOwnProperty('options')) {
+    options = extend({}, options, options.options);
+    delete options.options;
+  }
+
+  config.options = extend({}, config.options, options);
+  return config;
+}
+
+/**
+ * Object
+ */
+
+function normalizeObject(val) {
+  if (val.hasOwnProperty('path')) {
+    val.src = val.path;
+  }
+
+  val = normalizeOptions(val);
+
+  if (val.options && val.options.files) {
+    var config = {};
+    config.files = val.options.files;
+    delete val.options.files;
+    return normalize(config, val);
+  }
+
+  // if src/dest are on options, move them to root
+  val = utils.move(val, 'src');
+  val = utils.move(val, 'dest');
+
+  // allow src to be a getter
+  if ('src' in val || val.hasOwnProperty('dest')) {
+    return toFiles(val);
+  }
+
+  if (!val.hasOwnProperty('files')) {
+    return filesObjects(val);
+  }
+
+  if (Array.isArray(val.files)) {
+    return reduceFiles(val.files, val);
+  }
+
+  if (val.files && !utils.isObject(val.files)) {
+    throw new TypeError('expected "files" to be an array or object');
+  }
+
+  val.files = normalizeFiles(val);
+  return val;
+}
+
+/**
+ * Ensure that `src` on the given val is an array
+ *
+ * @param {Object} `val` Object with a `src` property
+ * @return {Object}
+ */
+
+function normalizeSrc(val) {
+  if (!val.src) return val;
+  val.src = utils.arrayify(val.src);
+  return val;
+}
+
+/**
+ * String
+ */
+
+function normalizeString(val) {
+  return toFiles({src: [val]});
+}
+
+/**
+ * Array
+ */
+
+function normalizeArray(arr) {
+  if (isValidSrc(arr[0])) {
+    return toFiles({src: arr});
+  }
+  return reduceFiles(arr);
+}
+
+/**
+ * Files property
+ */
+
+function normalizeFiles(val) {
+  var res = normalize(val.files);
+  return res.files;
+}
+
+/**
+ * Normalize all objects in a `files` array.
+ *
+ * @param {Array} `files`
+ * @return {Array}
+ */
+
+function reduceFiles(files, orig) {
+  var config = {files: []};
+  var len = files.length;
+  var idx = -1;
+
+  while (++idx < len) {
+    var val = normalize(files[idx]);
+    config.files = config.files.concat(val.files);
+  }
+
+  return copyNonfiles(config, orig);
+}
+
+/**
+ * Create a `files` array from a src-dest object.
+ *
+ * ```js
+ * // converts from:
+ * { src: '*.js', dest: 'foo/' }
+ *
+ * // to:
+ * { files: [{ src: ['*.js'], dest: 'foo/' }] }
+ * ```
+ */
+
+function copyNonfiles(config, provider) {
+  if (!provider) return config;
+  for (var key in provider) {
+    if (!isFilesKey(key)) {
+      config[key] = provider[key];
+    }
+  }
+  return config;
+}
+
+/**
  * Create a `files` array from a src-dest object.
  *
  * ```js
@@ -191,12 +238,7 @@ function copyOptions(config, thisArg) {
 
 function toFiles(val) {
   var config = {files: [normalizeSrc(val)]};
-  for (var key in val) {
-    if (!isFilesKey(key)) {
-      config[key] = val[key];
-    }
-  }
-  return config;
+  return copyNonfiles(config, val);
 }
 
 /**
@@ -230,19 +272,6 @@ function filesObjects(val) {
 }
 
 /**
- * Ensure that `src` on the given val is an array
- *
- * @param {Object} `val` Object with a `src` property
- * @return {Object}
- */
-
-function normalizeSrc(val) {
-  if (!val.src) return val;
-  val.src = utils.arrayify(val.src);
-  return val;
-}
-
-/**
  * Optionally sort the keys in all of the files objects.
  * Helps with debugging.
  *
@@ -264,15 +293,23 @@ function formatObject(val) {
     res[key] = val[key];
   }
 
-  var len = res.files.length, i = -1;
-  while (++i < len) {
-    var ele = res.files[i];
+  var len = res.files.length;
+  var idx = -1;
+
+  while (++idx < len) {
+    var ele = res.files[idx];
     var obj = {};
 
-    obj.options = ele.options || res.options;
+    obj.options = {};
     obj.src = ele.src || [];
     obj.dest = ele.dest || '';
-    res.files[i] = obj;
+
+    copyNonfiles(obj, ele);
+    if (!ele.options) {
+      obj.options = res.options;
+    }
+
+    res.files[idx] = obj;
   }
   return res;
 }
@@ -286,7 +323,13 @@ function isFilesKey(key) {
 }
 
 function isValidSrc(val) {
-  return typeof val === 'string' || Array.isArray(val);
+  if (typeof val === 'string') {
+    return true;
+  }
+  if (Array.isArray(val)) {
+    return val[0].src || val[0].dest;
+  }
+  return false;
 }
 
 /**
